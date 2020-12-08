4f48f7a0d7d0996284153b38f5b28eb8a3c625c8
damiencfs <damiencherfils@gmail.com>
2020-12-08 15:47:47 +0100

    modifs

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ let express = require('express');
 let app = express();
 let port = 3000;
 app.use(express.static(__dirname + '/www'));
-
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
+app.use('/js', express.static(__dirname + '/node_modules/popper.js/dist/umd'));
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 //Preparation du serveur 
 
 app.listen(port,()=>{
